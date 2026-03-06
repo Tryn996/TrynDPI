541f2885066d18ad2332cc3aca8434c888c782cd
Tryn996 <154067956+Tryn996@users.noreply.github.com>
2026-03-06 13:07:09 +0300

    Update panel.gd

--- script/scens/panel.gd
+++ script/scens/panel.gd
@@ -7,12 +7,10 @@ func _gui_input(event):
 	if event is InputEventMouseButton:
 		if event.button_index == MOUSE_BUTTON_LEFT:
 			if event.pressed:
-				dragging = true
-				# Запоминаем позицию мыши относительно ГЛОБАЛЬНОГО экрана
+				dragging =true
 				click_position = DisplayServer.mouse_get_position() - get_window().position
 			else:
 				dragging = false
 
 	if event is InputEventMouseMotion and dragging:
-		# Вычисляем новую позицию окна: текущая позиция мыши на экране МИНУС смещение клика
 		get_window().position = DisplayServer.mouse_get_position() - click_position

2892b40c2aa0d7ad9004be2b03d0ca5deb992df1
Tryn996 <154067956+Tryn996@users.noreply.github.com>
2026-03-31 01:46:09 +0300

    4.0

---
+++ BAT/reg_del.bat
@@ -0,0 +1,11 @@
+@echo off
+
+
+net session >nul 2>&1
+if %errorlevel% neq 0 (
+   echo 
+   powershell -Command "Start-Process '%~f0' -Verb RunAs"
+   exit
+)
+reg.exe DELETE HKLM\SOFTWARE\Microsoft\Windows\CurrentVersion\Run\ /v TrynDPI /f
+exit
